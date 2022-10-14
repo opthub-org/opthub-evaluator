d385fc9db9c73483ee61dfc82a111bbc6dfb0d76
hamada-n <hamada-n@klab.com>
2022-10-14 19:18:16 +0900

    black

--- example/sphere-2-2/sphere-2-2.py
+++ example/sphere-2-2/sphere-2-2.py
@@ -6,8 +6,8 @@ import json
 
 
 def sphere(x):
-    f1 = (x[0] - 1)**2 + x[1]**2
-    f2 = x[0]**2 + (x[1] - 1)**2
+    f1 = (x[0] - 1) ** 2 + x[1] ** 2
+    f2 = x[0] ** 2 + (x[1] - 1) ** 2
     return [f1, f2]
 
 
@@ -15,8 +15,8 @@ def main():
     x = input()
     variable = json.loads(x)
     objective = sphere(variable)
-    print(json.dumps({'objective': objective, 'constraint': None}))
+    print(json.dumps({"objective": objective, "constraint": None}))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- opthub_evaluator/main.py
+++ opthub_evaluator/main.py
@@ -29,17 +29,18 @@ class AliasedGroup(click.Group):
     ...     pass
     """
 
-    def get_command(self, ctx, cmd_name):  # pylint: disable=inconsistent-return-statements
+    def get_command(
+        self, ctx, cmd_name
+    ):  # pylint: disable=inconsistent-return-statements
         cmd = click.Group.get_command(self, ctx, cmd_name)
         if cmd is not None:
             return cmd
-        matches = [cmd for cmd in self.list_commands(ctx)
-                   if cmd.startswith(cmd_name)]
+        matches = [cmd for cmd in self.list_commands(ctx) if cmd.startswith(cmd_name)]
         if not matches:
             return None
         if len(matches) == 1:
             return click.Group.get_command(self, ctx, matches[0])
-        ctx.fail('Too many matches: %s' % ', '.join(sorted(matches)))
+        ctx.fail("Too many matches: %s" % ", ".join(sorted(matches)))
 
 
 class StrLength(StringParamType):
@@ -53,7 +54,9 @@ class StrLength(StringParamType):
     :param clamp: Clamp the input if exeeded
     """
 
-    def __init__(self, min=None, max=None, clamp=False):  # pylint: disable=redefined-builtin
+    def __init__(
+        self, min=None, max=None, clamp=False
+    ):  # pylint: disable=redefined-builtin
         self.min = min
         self.max = max
         self.clamp = clamp
@@ -63,27 +66,40 @@ class StrLength(StringParamType):
         len_ret = len(ret)
         if self.clamp:
             if self.min is not None and len_ret < self.min:
-                return ret + ' ' * (self.min - len_ret)
+                return ret + " " * (self.min - len_ret)
             if self.max is not None and len_ret > self.max:
-                return ret[:self.max]
-        if self.min is not None and len_ret < self.min or \
-           self.max is not None and len_ret > self.max:
+                return ret[: self.max]
+        if (
+            self.min is not None
+            and len_ret < self.min
+            or self.max is not None
+            and len_ret > self.max
+        ):
             if self.min is None:
                 self.fail(
-                    'Length %d is longer than the maximum valid length %d.'
-                    % (len_ret, self.max), param, ctx)
+                    "Length %d is longer than the maximum valid length %d."
+                    % (len_ret, self.max),
+                    param,
+                    ctx,
+                )
             elif self.max is None:
                 self.fail(
-                    'Length %d is shorter than the minimum valid length %d.'
-                    % (len_ret, self.min), param, ctx)
+                    "Length %d is shorter than the minimum valid length %d."
+                    % (len_ret, self.min),
+                    param,
+                    ctx,
+                )
             else:
                 self.fail(
-                    'Length %d is not in the valid range of %d to %d.'
-                    % (len_ret, self.min, self.max), param, ctx)
+                    "Length %d is not in the valid range of %d to %d."
+                    % (len_ret, self.min, self.max),
+                    param,
+                    ctx,
+                )
         return ret
 
     def __repr__(self):
-        return 'StrLength(%d, %d)' % (self.min, self.max)
+        return "StrLength(%d, %d)" % (self.min, self.max)
 
 
 def load_config(ctx, self, value):  # pylint:disable=unused-argument
@@ -110,7 +126,7 @@ def save_config(ctx, value):
     :return dict: Saveed config
     """
 
-    with open(value, 'w', encoding="utf-8") as file:
+    with open(value, "w", encoding="utf-8") as file:
         yaml.dump(ctx.default_map, file)
     return ctx.default_map
 
@@ -127,12 +143,12 @@ def query(ctx, gql_doc, **kwargs):
     For example, when gql_doc=Q_SOLUTION_TO_EVALUATE, response is about a single solution that
     has not been evaluated by objective functions.
     """
-    _logger.debug('query(%s, %s)', gql_doc, kwargs)
+    _logger.debug("query(%s, %s)", gql_doc, kwargs)
     try:
-        response = ctx.obj['client'].execute(gql(gql_doc), variable_values=kwargs)
+        response = ctx.obj["client"].execute(gql(gql_doc), variable_values=kwargs)
     except Exception as exc:
-        ctx.fail('Exception %s raised when executing query %s\n' % (exc, gql_doc))
-    _logger.debug('-> %s', response)
+        ctx.fail("Exception %s raised when executing query %s\n" % (exc, gql_doc))
+    _logger.debug("-> %s", response)
     return response
 
 
@@ -146,10 +162,10 @@ def wait_to_fetch(ctx, interval):
     """
     while True:
         response = query(ctx, Q_SOLUTION_TO_EVALUATE)  # Polling
-        if response['solutions']:
+        if response["solutions"]:
             break  # solution found
         sleep(interval)
-    return response['solutions'][0]['id']
+    return response["solutions"][0]["id"]
 
 
 def check_budget(ctx, user_id, match_id):
@@ -161,10 +177,12 @@ def check_budget(ctx, user_id, match_id):
     :raise Exception: When budget exceeded.
     """
     response = query(ctx, Q_CHECK_BUDGET, user_id=user_id, match_id=match_id)
-    progress = response['progress'][0]
-    n_eval = progress['submitted'] - progress['evaluation_error'] - progress['scoring_error']
-    if n_eval > progress['budget']:  # Budget exceeded.
-        raise Exception('Out of budget: %d / %d.' % (n_eval, progress['budget']))
+    progress = response["progress"][0]
+    n_eval = (
+        progress["submitted"] - progress["evaluation_error"] - progress["scoring_error"]
+    )
+    if n_eval > progress["budget"]:  # Budget exceeded.
+        raise Exception("Out of budget: %d / %d." % (n_eval, progress["budget"]))
 
 
 Q_SOLUTION_TO_EVALUATE = """
@@ -277,33 +295,65 @@ mutation cancel_evaluation(
 """
 
 
-@click.command(help='OptHub Evaluator.')
-@click.option('-u', '--url', envvar='OPTHUB_URL', type=str,
-              default='https://opthub-api.herokuapp.com/v1/graphql',
-              help='URL to OptHub.')
-@click.option('-a', '--apikey', envvar='OPTHUB_APIKEY',
-              type=StrLength(max=64), help='ApiKey.')
-@click.option('-i', '--interval', envvar='OPTHUB_INTERVAL',
-              type=click.IntRange(min=1), default=2, help='Polling interval.')
-@click.option('--verify/--no-verify', envvar='OPTHUB_VERIFY',
-              default=True, help='Verify SSL certificate.')
-@click.option('-r', '--retries', envvar='OPTHUB_RETRIES',
-              type=click.IntRange(min=0), default=3,
-              help='Retries to establish HTTPS connection.')
-@click.option('-t', '--timeout', envvar='OPTHUB_TIMEOUT',
-              type=click.IntRange(min=0), default=600,
-              help='Timeout to process a query.')
-@click.option('--rm', envvar='OPTHUB_REMOVE',
-              is_flag=True,
-              help='Remove containers after exit.')
-@click.option('-q', '--quiet', count=True, help='Be quieter.')
-@click.option('-v', '--verbose', count=True, help='Be more verbose.')
-@click.option('-c', '--config', envvar='OPTHUB_EVALUATOR_CONFIG',
-              type=click.Path(dir_okay=False), default='opthub-evaluator.yml',
-              is_eager=True, callback=load_config, help='Configuration file.')
+@click.command(help="OptHub Evaluator.")
+@click.option(
+    "-u",
+    "--url",
+    envvar="OPTHUB_URL",
+    type=str,
+    default="https://opthub-api.herokuapp.com/v1/graphql",
+    help="URL to OptHub.",
+)
+@click.option(
+    "-a", "--apikey", envvar="OPTHUB_APIKEY", type=StrLength(max=64), help="ApiKey."
+)
+@click.option(
+    "-i",
+    "--interval",
+    envvar="OPTHUB_INTERVAL",
+    type=click.IntRange(min=1),
+    default=2,
+    help="Polling interval.",
+)
+@click.option(
+    "--verify/--no-verify",
+    envvar="OPTHUB_VERIFY",
+    default=True,
+    help="Verify SSL certificate.",
+)
+@click.option(
+    "-r",
+    "--retries",
+    envvar="OPTHUB_RETRIES",
+    type=click.IntRange(min=0),
+    default=3,
+    help="Retries to establish HTTPS connection.",
+)
+@click.option(
+    "-t",
+    "--timeout",
+    envvar="OPTHUB_TIMEOUT",
+    type=click.IntRange(min=0),
+    default=600,
+    help="Timeout to process a query.",
+)
+@click.option(
+    "--rm", envvar="OPTHUB_REMOVE", is_flag=True, help="Remove containers after exit."
+)
+@click.option("-q", "--quiet", count=True, help="Be quieter.")
+@click.option("-v", "--verbose", count=True, help="Be more verbose.")
+@click.option(
+    "-c",
+    "--config",
+    envvar="OPTHUB_EVALUATOR_CONFIG",
+    type=click.Path(dir_okay=False),
+    default="opthub-evaluator.yml",
+    is_eager=True,
+    callback=load_config,
+    help="Configuration file.",
+)
 @click.version_option()
-@click.argument('command', envvar='OPTHUB_COMMAND',
-              type=str, nargs=-1)
+@click.argument("command", envvar="OPTHUB_COMMAND", type=str, nargs=-1)
 @click.pass_context
 def run(ctx, **kwargs):
     """The entrypoint of CLI.
@@ -312,137 +362,154 @@ def run(ctx, **kwargs):
     :param kwargs: GraphQL variables
     """
 
-    verbosity = 10 * (kwargs['quiet'] - kwargs['verbose'])
+    verbosity = 10 * (kwargs["quiet"] - kwargs["verbose"])
     log_level = logging.WARNING + verbosity
     logging.basicConfig(level=log_level)
-    _logger.info('Log level is set to %d', log_level)
-    _logger.debug('run(%s)', kwargs)
+    _logger.info("Log level is set to %d", log_level)
+    _logger.debug("run(%s)", kwargs)
     transport = RequestsHTTPTransport(
-        url=kwargs['url'],
-        verify=kwargs['verify'],
-        retries=kwargs['retries'],
-        headers={'X-Hasura-Admin-Secret': kwargs['apikey']},
+        url=kwargs["url"],
+        verify=kwargs["verify"],
+        retries=kwargs["retries"],
+        headers={"X-Hasura-Admin-Secret": kwargs["apikey"]},
     )
     ctx.obj = {
-        'client': Client(
+        "client": Client(
             transport=transport,
             fetch_schema_from_transport=True,
         )
     }
 
-    _logger.info('Connect to docker daemon...')
+    _logger.info("Connect to docker daemon...")
     client = docker.from_env()
-    _logger.info('...Connected')
+    _logger.info("...Connected")
 
     n_solution = 1
-    _logger.info('==================== Solution: %d ====================', n_solution)
+    _logger.info("==================== Solution: %d ====================", n_solution)
     while True:
         try:
-            _logger.info('Find solution to evaluate...')
-            solution_id = wait_to_fetch(ctx, kwargs['interval'])
+            _logger.info("Find solution to evaluate...")
+            solution_id = wait_to_fetch(ctx, kwargs["interval"])
             _logger.debug(solution_id)
-            _logger.info('...Found')
+            _logger.info("...Found")
         except Exception as exc:
             if isinstance(exc, InterruptedError):
                 _logger.info(exc)
-                _logger.info('Attempt graceful shutdown...')
-                _logger.info('No need to rollback')
-                _logger.info('...Shutted down')
+                _logger.info("Attempt graceful shutdown...")
+                _logger.info("No need to rollback")
+                _logger.info("...Shutted down")
                 ctx.exit(0)
             else:
                 _logger.error(format_exc())
                 continue
 
         try:
-            _logger.info('Try to lock solution to evaluate...')
+            _logger.info("Try to lock solution to evaluate...")
             response = query(ctx, Q_START_EVALUATION, id=solution_id)
-            if response['update_solutions']['affected_rows'] == 0:
-                _logger.info('...Already locked')
+            if response["update_solutions"]["affected_rows"] == 0:
+                _logger.info("...Already locked")
                 continue
-            if response['update_solutions']['affected_rows'] != 1:
-                _logger.error('Lock error: affected_rows must be 0 or 1, but %s', response)
-            solution = response['update_solutions']["returning"][0]
-            _logger.info('...Lock aquired')
+            if response["update_solutions"]["affected_rows"] != 1:
+                _logger.error(
+                    "Lock error: affected_rows must be 0 or 1, but %s", response
+                )
+            solution = response["update_solutions"]["returning"][0]
+            _logger.info("...Lock aquired")
 
-            _logger.info('Check budget...')
-            check_budget(ctx, user_id=solution['owner_id'], match_id=solution['match_id'])
-            _logger.info('...OK')
+            _logger.info("Check budget...")
+            check_budget(
+                ctx, user_id=solution["owner_id"], match_id=solution["match_id"]
+            )
+            _logger.info("...OK")
 
-            _logger.info('Parse variable to evaluate...')
-            _logger.debug(solution['variable'])
-            variable = json.dumps(solution['variable']) + '\n'
+            _logger.info("Parse variable to evaluate...")
+            _logger.debug(solution["variable"])
+            variable = json.dumps(solution["variable"]) + "\n"
             _logger.debug(variable)
-            _logger.info('...Parsed')
+            _logger.info("...Parsed")
 
-            _logger.info('Start container...')
-            _logger.debug(solution['match']['problem']['image'])
+            _logger.info("Start container...")
+            _logger.debug(solution["match"]["problem"]["image"])
             container = client.containers.run(
-                image=solution['match']['problem']['image'],
-                command=kwargs['command'],
-                environment={v['key']: v['value']
-                    for v in solution['match']['environments']},
+                image=solution["match"]["problem"]["image"],
+                command=kwargs["command"],
+                environment={
+                    v["key"]: v["value"] for v in solution["match"]["environments"]
+                },
                 stdin_open=True,
                 detach=True,
             )
-            _logger.info('...Started: %s', container.name)
+            _logger.info("...Started: %s", container.name)
 
-            _logger.info('Send variable...')
+            _logger.info("Send variable...")
             socket = container.attach_socket(
-                params={'stdin': 1, 'stream': 1, 'stdout': 1, 'stderr': 1}
+                params={"stdin": 1, "stream": 1, "stdout": 1, "stderr": 1}
             )
-            socket._sock.sendall(variable.encode('utf-8'))  # pylint: disable=protected-access
-            _logger.info('...Send')
+            socket._sock.sendall(
+                variable.encode("utf-8")
+            )  # pylint: disable=protected-access
+            _logger.info("...Send")
 
-            _logger.info('Wait for Evaluation...')
-            container.wait(timeout=kwargs['timeout'])
-            _logger.info('...Evaluated')
+            _logger.info("Wait for Evaluation...")
+            container.wait(timeout=kwargs["timeout"])
+            _logger.info("...Evaluated")
 
-            _logger.info('Recieve stdout...')
-            stdout = container.logs(stdout=True, stderr=False).decode('utf-8')
+            _logger.info("Recieve stdout...")
+            stdout = container.logs(stdout=True, stderr=False).decode("utf-8")
             _logger.debug(stdout)
-            _logger.info('...Recived')
+            _logger.info("...Recived")
 
-            if kwargs['rm']:
-                _logger.info('Remove container...')
+            if kwargs["rm"]:
+                _logger.info("Remove container...")
                 container.remove()
-                _logger.info('...Removed')
+                _logger.info("...Removed")
 
-            _logger.info('Parse stdout...')
+            _logger.info("Parse stdout...")
             stdout = json.loads(stdout)
             _logger.debug(stdout)
-            _logger.info('...Parsed')
+            _logger.info("...Parsed")
 
-            _logger.info('Check budget...')
-            check_budget(ctx, user_id=solution['owner_id'], match_id=solution['match_id'])
-            _logger.info('...OK')
+            _logger.info("Check budget...")
+            check_budget(
+                ctx, user_id=solution["owner_id"], match_id=solution["match_id"]
+            )
+            _logger.info("...OK")
 
-            _logger.info('Push evaluation...')
-            query(ctx, Q_FINISH_EVALUATION,
-                id=solution['id'],
-                objective=stdout.get('objective'),
-                constraint=stdout.get('constraint'),
-                info=stdout.get('info'),
-                error=stdout.get('error'))
-            _logger.info('...Pushed')
+            _logger.info("Push evaluation...")
+            query(
+                ctx,
+                Q_FINISH_EVALUATION,
+                id=solution["id"],
+                objective=stdout.get("objective"),
+                constraint=stdout.get("constraint"),
+                info=stdout.get("info"),
+                error=stdout.get("error"),
+            )
+            _logger.info("...Pushed")
         except Exception as exc:
             if isinstance(exc, InterruptedError):
                 _logger.info(exc)
-                _logger.info('Attempt graceful shutdown...')
-                _logger.info('Rollback evaluation...')
-                query(ctx, Q_CANCEL_EVALUATION, id=solution['id'])
-                _logger.info('...Rolled back')
-                _logger.info('...Shutted down')
+                _logger.info("Attempt graceful shutdown...")
+                _logger.info("Rollback evaluation...")
+                query(ctx, Q_CANCEL_EVALUATION, id=solution["id"])
+                _logger.info("...Rolled back")
+                _logger.info("...Shutted down")
                 ctx.exit(0)
             _logger.error(format_exc())
-            _logger.info('Finish evaluation...')
-            query(ctx, Q_FINISH_EVALUATION,
-                id=solution['id'],
+            _logger.info("Finish evaluation...")
+            query(
+                ctx,
+                Q_FINISH_EVALUATION,
+                id=solution["id"],
                 objective=None,
                 constraint=None,
                 info=None,
-                error=str(exc))
-            _logger.info('...Finished')
+                error=str(exc),
+            )
+            _logger.info("...Finished")
             continue
 
         n_solution += 1
-        _logger.info('==================== Solution: %d ====================', n_solution)
+        _logger.info(
+            "==================== Solution: %d ====================", n_solution
+        )
